d33130a05b4844d3de38d03f4f49d9628513ae57
Sophia DiPietro <sophiad.913@gmail.com>
2026-03-17 00:16:23 -0400

    Update submitForm.php

--- FoundUApp/viewListing/submitForm.php
+++ FoundUApp/viewListing/submitForm.php
@@ -1,8 +1,4 @@
 <!--****************************************************************************
- * COMP 3541 Final Project
- * Sophia DiPietro (T00714296)
- * March 17, 2026
- * 
  * FoundU - View Listing
  * 
  * submitForm.php
